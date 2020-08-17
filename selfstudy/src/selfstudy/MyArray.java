652ecdd1f2a2a7a7050e8250460e7f88140f7797
munsangu <ds-3632@hanmail.net>
2020-08-17 16:40:29 +0900

    20.08.17

---
+++ selfstudy/src/selfstudy/MyArray.java
@@ -0,0 +1,22 @@
+package selfstudy;
+public class MyArray 
+{
+	public static void main(String[] args) 
+	{
+		int[][]arr= {
+				{10,20,30,40},
+				{50,60},
+				{70,80,90}
+		};
+		System.out.println("배열의 행수: "+arr.length);
+		
+		for(int i=0;i<arr.length;i++) 
+		{
+			System.out.println((i+1)+"행의 열 수: "+arr[i].length);
+			for(int j=0;j<arr[i].length;j++) 
+			{
+				System.out.println("arr["+i+"]["+j+"] = "+arr[i][j]);
+			}
+		}
+	}
+}
