c4587fd5f2714d0d80cd6dd1c26285b27f008f7b
munsangu <ds-3632@hanmail.net>
2020-09-06 22:37:46 +0900

    test2

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -4,8 +4,12 @@ public class test {
 
 	public static void main(String[] args) 
 	{
-	
+		int a=10;
 		System.out.println("HEllo");
+		System.out.println(a+10);
+		System.out.println(a-10);
+		System.out.println(a*10);
+		System.out.println(a/10);
 	}
 
 }
