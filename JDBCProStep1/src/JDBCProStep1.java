86d5cad7af064e975e16b27a657047250944118f
munsangu <ds-3632@hanmail.net>
2020-09-10 17:30:06 +0900

    20.09.10

--- JDBCProStep1/src/JDBCProStep1.java
+++ JDBCProStep1/src/JDBCProStep1.java
@@ -1,13 +1,18 @@
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class JDBCProStep1 implements ActionListener {
 
@@ -24,6 +29,17 @@ public class JDBCProStep1 implements ActionListener {
 	private static final int SEARCH = 3;
 	private static final int TOTAL = 4;
 	int cmd = NONE;
+	String driver = "oracle.jdbc.OracleDriver";
+	String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	String user = "system";
+	String password = "123456";
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	String sqlTotal= "select * from customer";
+	String sqlInsert= "insert into customer values(?,?,?,?)";
+	String sqlDelete= "delete from customer where name=?";
+	String sqlUpdate= "update customer set email=? tel=? where code=?";
+	String sqlSearch= "select * from customer where name=?";
 	/**
 	 * Launch the application.
 	 */
@@ -42,10 +58,27 @@ public class JDBCProStep1 implements ActionListener {
 
 	/**
 	 * Create the application.
+	 * @throws Exception 
 	 */
-	public JDBCProStep1() {
+	public JDBCProStep1(){
 		initialize();
 		init();
+		dbcon();
+	}
+	
+	public void dbcon() 
+	{
+
+		try 
+		{
+		Class.forName(driver);
+		con = DriverManager.getConnection(url, user, password);
+		System.out.println("성공");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -102,28 +135,33 @@ public class JDBCProStep1 implements ActionListener {
 		scrollPane.setColumnHeaderView(table);
 		// 전체보기
 		btnTotal = new JButton("전체보기");
-		btnTotal.setBounds(81, 351, 97, 23);
+		btnTotal.setBounds(28, 351, 97, 23);
 		frame.getContentPane().add(btnTotal);
 		
 		//추가
 		btnAdd = new JButton("추가");
-		btnAdd.setBounds(202, 351, 97, 23);
+		btnAdd.setBounds(137, 351, 97, 23);
 		frame.getContentPane().add(btnAdd);
 	
 		//삭제
 		btnDel = new JButton("삭제");
-		btnDel.setBounds(328, 351, 97, 23);
+		btnDel.setBounds(246, 351, 97, 23);
 		frame.getContentPane().add(btnDel);
 
 		//검색
 		btnSearch = new JButton("검색");
-		btnSearch.setBounds(454, 351, 97, 23);
+		btnSearch.setBounds(355, 351, 97, 23);
 		frame.getContentPane().add(btnSearch);
 		
 		//취소
 		btnCancel = new JButton("취소");
-		btnCancel.setBounds(580, 351, 97, 23);
+		btnCancel.setBounds(573, 351, 97, 23);
 		frame.getContentPane().add(btnCancel);
+		
+		JButton button = new JButton("수정");
+		button.setEnabled(true);
+		button.setBounds(464, 351, 97, 23);
+		frame.getContentPane().add(button);
 		
 		btnTotal.addActionListener(this);
 		btnAdd.addActionListener(this);
@@ -132,21 +170,103 @@ public class JDBCProStep1 implements ActionListener {
 		btnCancel.addActionListener(this);
 		
 	}
+	// 추가버튼의 DB
+	private void add() 
+	{
+		System.out.println("추가");
+/*		System.out.print(txtNo.getText()+"  ");	
+		System.out.print(txtName.getText()+"  ");
+		System.out.print(txtEmail.getText()+"  ");
+		System.out.print(txtTel.getText()+"  ");*/
+		String no = txtNo.getText();
+		String name = txtName.getText();
+		String email = txtEmail.getText();
+		String tel = txtTel.getText();
+		try 
+		{
+			pstmt = con.prepareStatement(sqlInsert,ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			pstmt.setInt(1, Integer.valueOf(no));
+			pstmt.setString(2, name);
+			pstmt.setString(3, email);
+			pstmt.setString(4, tel);
+			int res = pstmt.executeUpdate();
+			if(res==1)System.out.println("성공");
+			else System.out.println("실패");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	// 삭제버튼의 DB
+	private void del() 
+	{
+		System.out.println("삭제");
+/*		System.out.print(txtNo.getText()+"  ");	
+		System.out.print(txtName.getText()+"  ");
+		System.out.print(txtEmail.getText()+"  ");
+		System.out.print(txtTel.getText()+"  ");*/
+		try 
+		{
+			pstmt = con.prepareStatement(sqlDelete,ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			pstmt.setString(1,txtName.getText());
+			int res = pstmt.executeUpdate();
+			if(res==1)System.out.println("성공");
+			else System.out.println("실패");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	// 검색버튼의 DB
+	private void search() 
+	{
+		System.out.println("검색");
+/*		System.out.print(txtNo.getText()+"  ");	
+		System.out.print(txtName.getText()+"  ");
+		System.out.print(txtEmail.getText()+"  ");
+		System.out.print(txtTel.getText()+"  ");*/
+		try 
+		{
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	// 전체보기버튼의 DB
+	private void total() 
+	{
+		System.out.println("전체보기");
+/*		System.out.print(txtNo.getText()+"  ");	
+		System.out.print(txtName.getText()+"  ");
+		System.out.print(txtEmail.getText()+"  ");
+		System.out.print(txtTel.getText()+"  ");*/
+		try 
+		{
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
 		if(e.getSource()==btnAdd) 
 		{
-
 			if(cmd!=ADD) 
 			{   
 				call(ADD);
 				return;
 			}
 			frame.setTitle("추가");
+			add();	// db에 연결하여 추가 작업이 됨
 		}
 		else if(e.getSource()==btnDel)
-			
 		{		
 		if(cmd!=DELETE) 
 		{
@@ -154,14 +274,17 @@ public class JDBCProStep1 implements ActionListener {
 			return;
 		}
 			frame.setTitle("삭제");
+			del();
 		}
 		else if(e.getSource()==btnSearch) 
-		{		if(cmd!=SEARCH) 
+		{		
+		if(cmd!=SEARCH) 
 		{
 			call(SEARCH);
 			return;
 		}
 			frame.setTitle("검색");
+			search();
 		}
 		else if(e.getSource()==btnTotal) 
 		{
@@ -169,14 +292,14 @@ public class JDBCProStep1 implements ActionListener {
 			{
 				call(TOTAL);
 			}
-			
 			frame.setTitle("전체보기");
+			total();
 		}	
 		System.out.println("취소");
 		call(NONE);
-		cmd=NONE;
 		init();
 	}
+
 	public void init() 
 	{
 		txtNo.setText("");
@@ -187,53 +310,45 @@ public class JDBCProStep1 implements ActionListener {
 		txtName.setEditable(false);
 		txtEmail.setEditable(false);
 		txtTel.setEditable(false);
+		btnTotal.setEnabled(true);
+		btnAdd.setEnabled(true);
+		btnDel.setEnabled(true);
+		btnSearch.setEnabled(true);
+		btnCancel.setEnabled(true);
 	}
-	public void call(int cmd) 
+	public void call(int command) 
 	{	// 텍스트 필드의 편집 상태 on/off
-		switch(cmd) 
+		btnTotal.setEnabled(false);
+		btnAdd.setEnabled(false);
+		btnDel.setEnabled(false);
+		btnSearch.setEnabled(false);
+		btnCancel.setEnabled(true);
+		switch(command) 
 		{
 		case ADD:
 			txtNo.setEditable(true);
 			txtName.setEditable(true);
 			txtEmail.setEditable(true);
 			txtTel.setEditable(true);
-			break;
-		case DELETE:
-		case SEARCH:
-			txtName.setEditable(true);
-			break;
-		}
-		// 버튼의 상태 on/off
-		btnTotal.setEnabled(false);
-		btnAdd.setEnabled(false);
-		btnSearch.setEnabled(false);
-		btnDel.setEnabled(false);
-		switch(cmd) 
-		{
-		case ADD:
 			btnAdd.setEnabled(true);
 			cmd = ADD;
 			break;
 		case DELETE:
+			txtName.setEditable(true);
 			btnDel.setEnabled(true);
 			cmd = DELETE;
 			break;
 		case SEARCH:
+			txtName.setEditable(true);
 			btnSearch.setEnabled(true);
 			cmd = SEARCH;
 			break;
 		case TOTAL:
-			btnTotal.setEnabled(true);
 			cmd = TOTAL;
 			break;
 		case NONE:
-		btnTotal.setEnabled(true);
-		btnAdd.setEnabled(true);
-		btnSearch.setEnabled(true);
-		btnDel.setEnabled(true);
-		btnCancel.setEnabled(true);
-		cmd=NONE;
-		break;
-		}
-	}
+			cmd=NONE;
+			break;		
+		}		
+	}	
 }
